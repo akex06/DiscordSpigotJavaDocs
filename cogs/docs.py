27ac5dfc9f61419fdfc082669bb5320341490544
Akex <67469268+akex06@users.noreply.github.com>
2023-02-13 16:29:49 +0100

    Add files via upload

--- cogs/docs.py
+++ cogs/docs.py
@@ -6,28 +6,52 @@ from discord.ext import commands
 class Docs(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.docs_url = "https://hub.spigotmc.org/javadocs/spigot"
+        self.docs = {
+            "spigot": "https://hub.spigotmc.org/javadocs/spigot",
+            "bungeecord": "https://ci.md-5.net/job/BungeeCord/ws/api/target/apidocs/",
+            "bungeecord-chat": "https://javadoc.io/doc/net.md-5/bungeecord-chat/latest/"
+        }
 
-    @commands.command(aliases = ["docs", ])
-    async def d(self, ctx: commands.Context, path: str = None) -> None:
-        if not path:
-            await ctx.send("`path` can't be None")
-            return
 
+
+    async def get_docs(self, javadocs: str, path: str) -> str:
         async with aiohttp.ClientSession() as session:
             if path.split(".")[-1][0].isupper():
-                url = f"{self.docs_url}/{path.replace('.', '/')}.html"
-                print(url)
+                url = f"{self.docs[javadocs]}/{path.replace('.', '/')}.html"
 
             else:
-                url = f"{self.docs_url}/{path.replace('.', '/')}/package-summary.html"
+                url = f"{self.docs[javadocs]}/{path.replace('.', '/')}/package-summary.html"
 
             async with session.get(url) as resp:
                 if resp.status == 200:
-                    await ctx.send(url)
+                    return url
 
                 else:
-                    await ctx.send("Please provide a valid path")
+                    return "Please provide a valid path"
+
+    @commands.command(aliases = ["jd-s", ])
+    async def jds(self, ctx: commands.Context, path: str = None) -> None:
+        if not path:
+            await ctx.send(self.docs["spigot"])
+            return
+            
+        await ctx.send(await self.get_docs("spigot", path))
+
+    @commands.command(aliases = ["jd-bc", ])
+    async def jdbc(self, ctx: commands.Context, path: str = None) -> None:
+        if not path:
+            await ctx.send(self.docs["bungeecord"] + "index.html")
+            return
+
+        await ctx.send(await self.get_docs("bungeecord", path))
+
+    @commands.command(aliases = ["jd-bcc", ])
+    async def jdbcc(self, ctx: commands.Context, path: str = None) -> None:
+        if not path:
+            await ctx.send(self.docs["bungeecord-chat"] + "index.html")
+            return
+
+        await ctx.send(await self.get_docs("bungeecord-chat", path))
 
 def setup(bot: commands.Bot):
     bot.add_cog(Docs(bot))
